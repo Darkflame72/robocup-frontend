78719dbd895b15b8a279a7d2046f481ba3d1628f
Leon Bowie <leon@bowie-co.nz>
2021-06-29 20:27:03 +1200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    "camelcase": "off",
+    "@typescript-eslint/camelcase": ["off"]
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ],
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ]
+}
